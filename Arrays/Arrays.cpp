01cdd3e8a6c4f99b4fb3c713cd5f3c1813b3aa13
vadim <vadim@DESKTOP-TQSCKM7>
2023-12-16 20:49:08 +0200

    Добавьте файлы проекта.

---
+++ Arrays/Arrays.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+
+void duplicateZeros(std::vector<int>& arr) {
+    int i = 0;
+    while (i < arr.size()) {
+        if (arr[i] == 0) {
+            arr.insert(arr.begin() + i, 0);
+            arr.pop_back();
+            i += 2; // пропускаємо новий дубльований елемент
+        }
+        else {
+            i += 1;
+        }
+    }
+}
+
+int main() {
+    
+    std::vector<int> arr1 = { 1, 0, 2, 3, 0, 4, 5, 0 };
+    duplicateZeros(arr1);
+    for (const auto& num : arr1) {
+        std::cout << num << " ";
+    }
+    
+
+    
+
+    
+    return 0;
+}
